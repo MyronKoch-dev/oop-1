8a0caccd991f29d059ca24befadeb139451324f9
Myron Koch <myronkoch.dev@gmail.com>
2025-04-10 03:17:45 -0400

    **Commit Message:**
    - Update `--background` to `oklch(0 0 0)` and `240 10% 3.9%` in `globals.css`
    - Add `bg-black` class to `body` in `layout.tsx`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
       >
         <ThemeProvider>
           {/* Left Sidebar toggle button - visible only on mobile */}
